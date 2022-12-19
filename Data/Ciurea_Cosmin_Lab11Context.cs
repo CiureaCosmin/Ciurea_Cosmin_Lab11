1cf3325350486d03d5072d058350c4e4ca9b7266
CiureaCosmin <75529179+CiureaCosmin@users.noreply.github.com>
2022-12-19 19:21:56 +0200

    Add project files.

---
+++ Data/Ciurea_Cosmin_Lab11Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ciurea_Cosmin_Lab11.Models;
+
+namespace Ciurea_Cosmin_Lab11.Data
+{
+    public class Ciurea_Cosmin_Lab11Context : DbContext
+    {
+        public Ciurea_Cosmin_Lab11Context (DbContextOptions<Ciurea_Cosmin_Lab11Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ciurea_Cosmin_Lab11.Models.ShopList> ShopList { get; set; } = default!;
+    }
+}
